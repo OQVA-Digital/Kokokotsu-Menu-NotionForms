2f18c8911da93c18db5017f235d7048f7309e8b1
OQVA Digital <116745354+OQVA-Digital@users.noreply.github.com>
2022-12-08 14:00:27 -0300

    Add files via upload

--- gyoza-san.js
+++ gyoza-san.js
@@ -12,14 +12,18 @@ gyozaSanBox.addEventListener("click", function() {
 
             gyozaRainCtnr.style.opacity = 1
             
-            allGyozas[i].style.animation = "gyoza_fall " + (Math.random() * (6.5 - 3.5 + 1) + 3.5) +  "s ease-out infinite";
+            allGyozas[i].style.animation = "gyoza_fall " + (Math.random() * (10 - 2.5 + 1) + 2.5) +  "s ease-out infinite";
 
-            allGyozas[i].style.animationDelay = (Math.random() * (10 - 0 + 1) + 0) +  "s";
+            allGyozas[i].style.animationDelay = (Math.random() * (15 - 1 + 1) + 0) +  "s";
             
-            allGyozas[i].style.top = Math.floor(Math.random() * (180 - 0 + 1) + 0) +  "vh";
+            allGyozas[i].style.top = Math.random() * (180 - 0 + 1) + 0 +  "vh";
             
-            allGyozas[i].style.width = (Math.floor(Math.random() * (10 - 4 + 1) + 4) * 1.6) +  "vh";
-            allGyozas[i].style.height = Math.floor(Math.random() * (10 - 4 + 1) + 4) +  "vh";
+            allGyozas[i].style.width = Math.random() * (12 - 4 + 1) + 4 * 1.6 +  "vh";
+            allGyozas[i].style.height = Math.random() * (12 - 4 + 1) + 4 +  "vh";
+            
+            // allGyozas[i].style.filter = "blur(" + (Math.random() * (0.15 - 0) + 0) +  "rem) brightness(" + (Math.random() * (1 - 0.3) + 0.3) + ")";
+            
+            allGyozas[i].style.filter = "brightness(" + (Math.random() * (1 - 0.4) + 0.4) + ")";
     
             itsRaining = true;
         }
